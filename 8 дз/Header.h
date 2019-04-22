6c48f56969760c0eb3f5e2a14e640f2cbc4e3489
Helena1995 <48977304+Helena1995@users.noreply.github.com>
2019-04-23 00:06:16 +0900

    Домашнее задание 8

---
+++ 8 дз/Header.h
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Money
+{
+private:
+	long rubley;
+	unsigned char kopeek;
+
+public:
+	void set(int newRubley, int newKopek);
+	long getRubley();
+	unsigned char getKopeek();
+	void addMoney(Money& someMoney );
+	void withdrawMoney(Money& someMoney);
+	void print();
+
+};
